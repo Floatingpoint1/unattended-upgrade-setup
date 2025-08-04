b10f9c6fdafc17035023935d92aee21ce4281d6f
R@b!t <42339766+Floatingpoint1@users.noreply.github.com>
2025-08-04 15:17:48 +0200

    0.0.2

--- test/test-dry-run.sh
+++ test/test-dry-run.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+# Testskript für Unattended-Upgrades im Dry-Run-Modus
+set -euo pipefail
+
+echo "[*] Starte Dry-Run Test für unattended-upgrade..."
 unattended-upgrade --dry-run --debug
